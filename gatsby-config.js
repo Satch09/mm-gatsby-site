2e11255bedaf0b598c1a6eede89226c43bb954a0
satch09 <18578121+Satch09@users.noreply.github.com>
2020-06-20 17:36:01 +0200

    turn off sitemap for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     ],
   },
   plugins: [
-    `gatsby-plugin-sitemap`,
+    // `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
