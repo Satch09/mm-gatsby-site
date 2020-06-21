c780d5d8631aa400080feadc2c1caa5ddbb684b3
satch09 <18578121+Satch09@users.noreply.github.com>
2020-06-21 11:42:55 +0200

    add missing useEffect to pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MessageBox from "components/MessageBox";
 
 import HeaderPicture from "components/HeaderPicture";
--- src/pages/training.jsx
+++ src/pages/training.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 
 import CardTraining from "components/Cards/CardTraining";
